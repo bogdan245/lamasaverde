efd58c112055908f51aafed980b27e0c37c05603
bogdan245 <pmbogdan96@gmail.com>
2022-11-01 15:41:55 +0200

    Added post creation

--- footblog/foot_space/urls.py
+++ footblog/foot_space/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView, ArticleDetail
+from .views import HomeView, ArticleDetail, AddPost
 
 
 urlpatterns = [
     # path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetail.as_view(), name='article-detail'),
+    path('add_post/', AddPost.as_view(), name='add_post'),
 ]
