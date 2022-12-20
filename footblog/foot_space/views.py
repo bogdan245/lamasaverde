976f7f325ce754dbb4c310d48f7bb659a95c4057
bogdan245 <pmbogdan96@gmail.com>
2022-12-20 10:16:29 +0200

    ordering

--- footblog/foot_space/views.py
+++ footblog/foot_space/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post, Category
 from .form import PostForm
 from django.urls import reverse_lazy
@@ -8,12 +14,13 @@ import datetime
 # def home(request):
 #     return render(request, 'home.html', {})
 
+
 class HomeView(ListView):
     model = Post
     template_name = "home.html"
     # ordering = ['-id']
     cats = Category.objects.all()
-    ordering = ['-post_date']
+    ordering = ["-post_date"]
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
@@ -22,17 +29,16 @@ class HomeView(ListView):
         return context
 
 
-
-
-
 def CategoryView(request, cats):
-    category_post = Post.objects.filter(category=cats.replace("-", ' ')) #category e definit in model
+    category_post = Post.objects.filter(
+        category=cats.replace("-", " ")
+    )  # category e definit in model
     context = {}
-    context['cats'] = cats.title().replace('-', ' ')
-    context['category_post'] = category_post
+    context["cats"] = cats.title().replace("-", " ")
+    context["category_post"] = category_post
     cat_menu = Category.objects.all()
-    context['cat_menu'] = cat_menu
-    return render(request, 'categories.html', context)
+    context["cat_menu"] = cat_menu
+    return render(request, "categories.html", context)
 
 
 # def CategoryListView(request, cats):
@@ -40,10 +46,10 @@ def CategoryView(request, cats):
 #     return render(request, 'categories.html', {'cat_menu_list': cat_menu_list})
 
 
-
 class ArticleDetail(DetailView):
     model = Post
-    template_name = 'article_details.html'
+    template_name = "article_details.html"
+
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(ArticleDetail, self).get_context_data(*args, **kwargs)
@@ -54,18 +60,21 @@ class ArticleDetail(DetailView):
 class AddPost(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'add_post.html'
+    template_name = "add_post.html"
+
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(AddPost, self).get_context_data(*args, **kwargs)
         context["cat_menu"] = cat_menu
         return context
 
+
 class AddCategory(CreateView):
     model = Category
     form_class = PostForm
-    template_name = 'add_category.html'
-    fields = '__all__'
+    template_name = "add_category.html"
+    fields = "__all__"
+
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(AddCategory, self).get_context_data(*args, **kwargs)
@@ -76,7 +85,7 @@ class AddCategory(CreateView):
 class EditPost(UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'update_post.html'
+    template_name = "update_post.html"
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
@@ -84,10 +93,12 @@ class EditPost(UpdateView):
         context["cat_menu"] = cat_menu
         return context
 
+
 class DeletePost(DeleteView):
     model = Post
-    template_name = 'delete_post.html'
-    success_url = reverse_lazy('home')
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("home")
+
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(DeletePost, self).get_context_data(*args, **kwargs)
